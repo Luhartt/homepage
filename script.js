3412a8dd0ba4cc1eff4d78d94a7fbcd3a31dfe76
Charles Nathaniel Togle <149758871+Luhartt@users.noreply.github.com>
2024-08-12 21:04:14 +0800

    initial upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function setTime() {
+  let suffix;
+  const displayHours = document.querySelector("div#time>span.hours");
+  const displayMinutes = document.querySelector("div#time>span.minutes");
+  let currentTime = new Date();
+  const hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+
+  minutes = minutes < 10 ? "0" + minutes : minutes
+  suffix = hours < 12 ? "AM" : "PM"
+
+  displayHours.innerHTML = hours;
+  displayMinutes.innerHTML = minutes + " " + suffix;
+  setTimeout(setTime, 50000);
+}
+
+function setColon() {
+  const colon = document.getElementById("colon")
+  colon.classList.toggle("blink");
+  setTimeout(setColon, 1000);
+}
+
+setColon();
+setTime();
+
